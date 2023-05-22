f483f0d6f5ab939217c426115fc0bba411356b27
turtleluc <turtleluczuidema@gmail.com>
2023-05-22 19:32:08 +0200

    Money System

--- Assets/Script/Target.cs
+++ Assets/Script/Target.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Target : MonoBehaviour
 {
+    public int MoneyPlus = 10;
     public float health = 50f;
     public void Takedamage(float amount)
     {
@@ -11,6 +13,8 @@ public class Target : MonoBehaviour
         if(health <= 0f)
         {
             Die();
+            Money.moneyValue += MoneyPlus;
+
         }
     }
 
