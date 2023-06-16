57534ef2e820a9a98cc043e9ddeb8773c1ac397b
RTVHypeGamer <114234122+RTVHypeGamer@users.noreply.github.com>
2023-06-16 11:03:39 +0200

    settings menu quality setting

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -6,8 +6,14 @@ using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour
 {
     public Slider Volume;
+
     public void SetVolume()
     {
         Debug.Log(Volume.value);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
 }
