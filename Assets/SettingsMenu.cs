017fdbd3af7d61ff47f361fbc7b3060bee351838
turtleluc <turtleluczuidema@gmail.com>
2023-06-20 09:17:56 +0200

    Yanick menu sript opgelost

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -24,7 +24,7 @@ public class SettingsMenu : MonoBehaviour
         int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++) 
         {
-            string option = resolutions[i].width + " x " + resolutions[i].heigth;
+            string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
 
             if (resolutions[i].width == Screen.currentResolution.width &&
